e6e0213a96860c2f359e47d23a7b5985246a82f1
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-10-18 14:01:03 +0200

    integrate echarts as charting library. also prepare a first static showcase bar diagram initially

--- app/components/wpsUserInterface/wpsControls/wpsExecute/wps-execute.component.js
+++ app/components/wpsUserInterface/wpsControls/wpsExecute/wps-execute.component.js
@@ -15,5 +15,32 @@ angular
 									wpsFormControlService) {
 								this.wpsPropertiesServiceInstance = wpsPropertiesService;
 								this.wpsFormControlServiceInstance = wpsFormControlService;
+
+								// based on prepared DOM, initialize echarts instance
+				        var myChart = echarts.init(document.getElementById('barDiagram'));
+
+				        // specify chart configuration item and data
+				        var option = {
+				            title: {
+				                text: 'ECharts entry example'
+				            },
+				            tooltip: {},
+				            legend: {
+				                data:['Sales']
+				            },
+				            xAxis: {
+				                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+				            },
+				            yAxis: {},
+				            series: [{
+				                name: 'Sales',
+				                type: 'bar',
+				                data: [5, 20, 36, 10, 10, 20]
+				            }]
+				        };
+
+				        // use configuration item and data specified to show chart
+				        myChart.setOption(option);
+
 							} ]
 				});
