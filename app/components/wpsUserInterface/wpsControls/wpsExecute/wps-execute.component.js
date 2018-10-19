0404f51659ac4e20ec3de10b00c7c4387458275f
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-10-19 17:17:19 +0200

    resize histogram diagram width and also change tooltip type to line

--- app/components/wpsUserInterface/wpsControls/wpsExecute/wps-execute.component.js
+++ app/components/wpsUserInterface/wpsControls/wpsExecute/wps-execute.component.js
@@ -155,7 +155,7 @@ angular
 										tooltip: {
 													trigger: 'axis',
 													axisPointer: {
-															type: 'cross',
+															type: 'line',
 															crossStyle: {
 																	color: '#999'
 															}
