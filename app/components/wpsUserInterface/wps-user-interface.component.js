e2c0d6b43ce00188649dd6ebe659c4c77bca86f7
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-11-06 15:03:02 +0100

    add better control and visuals of selected sideBar button and ensure only one is active at the same time

--- app/components/wpsUserInterface/wps-user-interface.component.js
+++ app/components/wpsUserInterface/wps-user-interface.component.js
@@ -7,16 +7,27 @@ angular.module('wpsUserInterface').component('wpsUserInterface', {
 		$scope.sidebarIndicatorConfigClass = "";
 		$scope.sidebarDiagramsClass = "hidden";
 
+		$scope.buttonIndicatorConfigClass = "btn btn-custom btn-docked";
+		$scope.buttonDiagramsClass = "btn btn-custom btn-circle";
+
+		$scope.undockButtons = function(){
+			$scope.buttonIndicatorConfigClass = "btn btn-custom btn-circle";
+			$scope.buttonDiagramsClass = "btn btn-custom btn-circle";
+		};
+
 		$scope.hideSidebars = function(){
 			$scope.sidebarIndicatorConfigClass = "hidden";
 			$scope.sidebarDiagramsClass = "hidden";
 		};
 
 		$scope.onSidebarIndicatorButtonClick = function(){
+			$scope.undockButtons();
 
 			if($scope.sidebarIndicatorConfigClass === "hidden"){
 				$scope.hideSidebars();
+				$scope.undockButtons();
 				$scope.sidebarIndicatorConfigClass = "";
+				$scope.buttonIndicatorConfigClass = "btn btn-custom btn-docked";
 			}
 			else{
 				$scope.sidebarIndicatorConfigClass = "hidden";
@@ -24,9 +35,11 @@ angular.module('wpsUserInterface').component('wpsUserInterface', {
 		}
 
 		$scope.onSidebarDiagramsClick = function(){
+			$scope.undockButtons();
 			if($scope.sidebarDiagramsClass === "hidden"){
 				$scope.hideSidebars();
 				$scope.sidebarDiagramsClass = "";
+				$scope.buttonDiagramsClass = "btn btn-custom btn-docked";
 			}
 			else{
 				$scope.sidebarDiagramsClass = "hidden";
