30285c7746503ab95238fe06ce3d18591f73ade9
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-11-06 14:40:23 +0100

    add another navbar sidebar for the diagrams

--- app/components/wpsUserInterface/wps-user-interface.component.js
+++ app/components/wpsUserInterface/wps-user-interface.component.js
@@ -5,15 +5,32 @@ angular.module('wpsUserInterface').component('wpsUserInterface', {
 		this.wpsPropertiesServiceInstance = wpsPropertiesService;
 
 		$scope.sidebarIndicatorConfigClass = "";
+		$scope.sidebarDiagramsClass = "hidden";
+
+		$scope.hideSidebars = function(){
+			$scope.sidebarIndicatorConfigClass = "hidden";
+			$scope.sidebarDiagramsClass = "hidden";
+		};
 
 		$scope.onSidebarIndicatorButtonClick = function(){
-			if($scope.sidebarIndicatorConfigClass === "active"){
+
+			if($scope.sidebarIndicatorConfigClass === "hidden"){
+				$scope.hideSidebars();
 				$scope.sidebarIndicatorConfigClass = "";
 			}
 			else{
-				$scope.sidebarIndicatorConfigClass = "active";
+				$scope.sidebarIndicatorConfigClass = "hidden";
 			}
+		}
 
+		$scope.onSidebarDiagramsClick = function(){
+			if($scope.sidebarDiagramsClass === "hidden"){
+				$scope.hideSidebars();
+				$scope.sidebarDiagramsClass = "";
+			}
+			else{
+				$scope.sidebarDiagramsClass = "hidden";
+			}
 		}
 
 	}
