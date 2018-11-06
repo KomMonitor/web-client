cdcc5923dfcd6814b4eb73416def6173200bcd79
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-11-06 19:59:10 +0100

    modify glyphicons and hide sidebar on startup

--- app/components/wpsUserInterface/wps-user-interface.component.js
+++ app/components/wpsUserInterface/wps-user-interface.component.js
@@ -4,12 +4,12 @@ angular.module('wpsUserInterface').component('wpsUserInterface', {
 
 		this.wpsPropertiesServiceInstance = wpsPropertiesService;
 
-		$scope.sidebarIndicatorConfigClass = "";
+		$scope.sidebarIndicatorConfigClass = "hidden";
 		$scope.sidebarDiagramsClass = "hidden";
 		$scope.sidebarRadarDiagramClass = "hidden";
 		$scope.sidebarProcessingClass = "hidden";
 
-		$scope.buttonIndicatorConfigClass = "btn btn-custom btn-docked";
+		$scope.buttonIndicatorConfigClass = "btn btn-custom btn-circle";
 		$scope.buttonDiagramsClass = "btn btn-custom btn-circle";
 		$scope.buttonRadarDiagramClass = "btn btn-custom btn-circle";
 		$scope.buttonProcessingClass = "btn btn-custom btn-circle";
