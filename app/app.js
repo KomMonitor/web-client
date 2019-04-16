2b2aa6a7f564b3c3b2a31d570423ec55b2b8d9b6
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-04-16 12:55:52 +0200

    disclaimer and popup for IE and Edge

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
+
+if (/MSIE 9/i.test(navigator.userAgent) || /MSIE 10/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+    // This is internet explorer 9, 10 or 11
+    window.alert('Sie verwenden einen veralteten Browser. KomMonitor kann in diesem Browser nicht korrekt dargestellt werden. Bitte nutzen Sie einen modernen Browser. Empfohlen werden Firefox, Chrome.');
+}
+
+
+if (/Edge\/\d./i.test(navigator.userAgent)){
+   // This is Microsoft Edge
+   window.alert('Sie verwenden Microsoft Edge. KomMonitor wird derzeit in diesem Browser nicht korrekt dargestellt. Bitte nutzen Sie einen anderen modernen Browser. Empfohlen werden Firefox, Chrome. Nach Schließen des Fensters startet die Anwendung, einige Elemente und Funktionen werden jedoch nicht vollständig unterstützt.');
+}
+
 var env = {};
 
 // Import variables if present (from env.js)
