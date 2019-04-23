f292f5dbf7d97ea27f39057243853040d79049a8
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-04-23 11:53:35 +0200

    first draft implementation of contact form

--- app/components/kommonitorUserInterface/kommonitorControls/feedbackModal/feedback-modal.component.js
+++ app/components/kommonitorUserInterface/kommonitorControls/feedbackModal/feedback-modal.component.js
@@ -4,5 +4,54 @@ angular.module('feedbackModal').component('feedbackModal', {
 
 		this.kommonitorDataExchangeServiceInstance = kommonitorDataExchangeService;
 
+		$scope.titel;
+		$scope.organization;
+		$scope.contactDetails;
+		$scope.feedbackType = "Frage";
+		$scope.feedbackContent;
+		$scope.like = "gut";
+
+		$scope.onSubmit = function(){
+				var body = "Titel:  " + $scope.titel + "\n";
+				body += "Fachbereich/Organisation:  " + $scope.organization + "\n\n";
+				body += "Kontaktdaten:  " + $scope.contactDetails + "\n\n";
+				body += "Feedback Typ:  " + $scope.feedbackType + "\n\n";
+				body += "Feedback Inhalt:  " + $scope.feedbackContent + "\n\n";
+				body += "KomMonitor Bewertung:  " + $scope.like + "\n\n";
+
+				$scope.sendMail(body);
+		};
+
+		$scope.sendMail = function(body){
+
+			if(! $scope.validate()){
+				return;
+			}
+
+		  var link = "mailto:christian.danowski-buhren@hs-bochum.de"+
+		             "?"+
+		             "subject=" + encodeURIComponent("KomMonitor - Feedback") +
+		             "&body=" + encodeURIComponent(body);
+		  window.location.href = link;
+
+			// console.log(link);
+		}
+
+		$scope.validate = function(){
+			if ($scope.titel && $scope.organization && $scope.contactDetails && $scope.feedbackType && $scope.feedbackContent && $scope.like){
+				return true;
+			}
+			return false;
+		}
+
+		$scope.reset = function(){
+			$scope.titel = undefined;
+			$scope.organization = undefined;
+			$scope.contactDetails = undefined;
+			$scope.feedbackType = "Frage";
+			$scope.feedbackContent = undefined;
+			$scope.like = "gut";
+		}
+
 	}
 ]});
