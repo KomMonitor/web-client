dcda918ee1ff0ead3ac2cd106763d9e29ff7b101
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-05-08 15:38:32 +0200

    allow users to send an optional attachment in feedback mails

--- app/components/kommonitorUserInterface/kommonitorControls/feedbackModal/feedback-modal.component.js
+++ app/components/kommonitorUserInterface/kommonitorControls/feedbackModal/feedback-modal.component.js
@@ -13,12 +13,26 @@ angular.module('feedbackModal').component('feedbackModal', {
 		$scope.feedbackType = undefined;
 		$scope.feedbackContent;
 		$scope.like = undefined;
-		$scope.attachement = undefined;
+		$scope.attachment = undefined;
+		document.getElementById("attachment").value = "";
 
 		$scope.error = undefined;
 		$scope.success = undefined;
 
-		$scope.onSubmit = function(){
+		var fileToBase64 = (file) => {
+			  return new Promise(resolve => {
+			    var reader = new FileReader();
+			    // Read file content on file loaded event
+			    reader.onload = function(event) {
+			      resolve(event.target.result);
+			    };
+
+			    // Convert data to base64
+			    reader.readAsDataURL(file);
+			  });
+			};
+
+		$scope.onSubmit = async function(){
 				// var body = "Titel:  " + $scope.titel + "\n";
 				var body = "Fachbereich/Organisation:  " + $scope.organization + "\n\n";
 				body += "Kontaktdaten:  " + $scope.contactDetails + "\n\n";
@@ -26,11 +40,31 @@ angular.module('feedbackModal').component('feedbackModal', {
 				body += "Feedback Inhalt:  " + $scope.feedbackContent + "\n\n";
 				body += "KomMonitor Bewertung:  " + $scope.like + "\n\n";
 
+				// var blobFile = $('#attachment').files[0];
+				var files = document.getElementById('attachment').files;
+				if(files.length > 0){
+					var uploadFile = files[0];
+					var base64 = await fileToBase64(uploadFile);
+					$scope.attachment = base64;
+				}
+
+
+		    // var formData = undefined;
+				//
+				// if(files.length > 0){
+				// 	formData = new FormData();
+				//
+				// 	for (var i = 0; i < files.length; i++) {
+				// 		formData.append("files[]", files[i]);
+				// 	}
+				//
+				// }
+
 				var mailInput = {};
 				mailInput.recipientMail = feedbackMailRecipient;
 				mailInput.subject = "KomMonitor - Feedback";
 				mailInput.body = body;
-				mailInput.attachement = $scope.attachement;
+				mailInput.attachment = $scope.attachment;
 
 				$scope.sendMail(mailInput);
 		};
@@ -90,7 +124,8 @@ angular.module('feedbackModal').component('feedbackModal', {
 			$scope.feedbackType = undefined;
 			$scope.feedbackContent = undefined;
 			$scope.like = undefined;
-			$scope.attachement = undefined;
+			$scope.attachment = undefined;
+			document.getElementById("attachment").value = "";
 
 			$scope.error = undefined;
 			$scope.success = undefined;
