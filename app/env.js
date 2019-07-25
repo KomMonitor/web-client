018fd3953c1503126043ee31c8d0330dfe809fe9
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-07-25 10:43:44 +0200

    add comments

--- app/env.js
+++ app/env.js
@@ -5,40 +5,56 @@
   // Setting this to false will disable console output
   window.__env.enableDebug = true;
 
-  // property names of feature id and name (relevant for all spatial features) - KomMonitor specific 
+  // property names of feature id and name (relevant for all spatial features) - KomMonitor specific
+  // DO NOT CHANGE THEM - ONLY IF YOU REALLY KNOW WHAT YOU ARE DOING
   window.__env.FEATURE_ID_PROPERTY_NAME = "ID";
   window.__env.FEATURE_NAME_PROPERTY_NAME = "NAME";
+  window.__env.indicatorDatePrefix = "DATE_";
 
-  // API url
+  // Data Management API URL
   // window.__env.apiUrl = 'http://kommonitor.fbg-hsbo.de/';
   window.__env.apiUrl = 'http://localhost:8085/';
-  // Base url
+  // Base url for Data Management API
   window.__env.basePath = 'management';
 
+  // Processing Engine URL
   // window.__env.apiUrl = 'http://kommonitor.fbg-hsbo.de/processing';
   window.__env.targetUrlToProcessingEngine = 'http://localhost:8086/processing/';
+
+  // Open Route Service URL
     // window.__env.apiUrl = 'http://kommonitor.fbg-hsbo.de/openrouteservice';
   window.__env.targetUrlToReachabilityService_ORS = 'http://localhost:8090/openrouteservice-4.7.2';
+
+  // Open Trip Planner URL - currently not integrated
   // window.__env.apiUrl = 'http://kommonitor.fbg-hsbo.de/opentripplanner';
   window.__env.targetUrlToReachabilityService_OTP = 'http://localhost:8090/opentripplanner';
 
+  // optional geometry simplification (a feature of Data Management API)
   window.__env.simplifyGeometriesParameterName = "simplifyGeometries";
+  // allowed values and meaning:
+  // ["original" --> no simplification; "weak" --> weak simplification,
+  // "medium" --> medium simplification; "strong" --> string simplification]
   window.__env.simplifyGeometriesOptions = [{"label": "nein", "value": "original"}, {"label": "schwach", "value": "weak"}, {"label": "mittel", "value": "medium"}, {"label": "stark", "value": "strong"}];
+  // use strong as default to minimize size of queried features
+  // for display, strong simplification is okay
   window.__env.simplifyGeometries = "strong";
 
-  window.__env.indicatorDatePrefix = "DATE_";
-
+  // number of decimals for display of numeric values in app
   window.__env.numberOfDecimals = 2;
 
+  // starting viewpoint parameters and zoom level
   window.__env.initialLatitude = 51.4386432;
   window.__env.initialLongitude = 7.0115552;
   window.__env.initialZoomLevel = 12;
   window.__env.minZoomLevel = 11;
   window.__env.maxZoomLevel = 22;
 
+  // starting indicator and spatial unit
+  // if faulty values are provided, a random indicator will be displayed
   window.__env.initialIndicatorId = "d6f447c1-5432-4405-9041-7d5b05fd9ece";
   window.__env.initialSpatialUnitName = "Stadtteilebene";
 
+ // various color settings
   window.__env.defaultColorForNoDataValues = "black";
   window.__env.defaultBorderColorForNoDataValues = "black";
   window.__env.defaultColorForOutliers_high = "#191919";
@@ -60,17 +76,21 @@
   window.__env.defaultFillOpacityForHighlightedFeatures = "0.8";
   window.__env.useTransparencyOnIndicator = true;
   window.__env.useOutlierDetectionOnIndicator = false;
-  //allowesValues: equal_interval, quantile, jenks
-  window.__env.defaultClassifyMethod = "equal_interval";
 
+  // default color for specific classification as ColorBrewer palette name
+  // i.e. balance mode
+  // i.e. measure of value classification (German: Schwellwertklassifizierung)
   window.__env.defaultColorBrewerPaletteForBalanceIncreasingValues = "Purples";
   window.__env.defaultColorBrewerPaletteForBalanceDecreasingValues = "YlOrBr";
   window.__env.defaultColorBrewerPaletteForGtMovValues = "YlOrBr";
   window.__env.defaultColorBrewerPaletteForLtMovValues = "Blues";
 
+  // classification
+  //allowesValues: equal_interval, quantile, jenks
+  window.__env.defaultClassifyMethod = "equal_interval";
+
+  // e-mail recipient for feedback mail
   window.__env.feedbackMailRecipient = "thomas.blasche@amt62.essen.de";
   // window.__env.feedbackMailRecipient = "christian.danowski-buhren@hs-bochum.de";
-
-
 
 }(this));
