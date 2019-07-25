850570e734a8fbe09ad92cfbecc7280f0887bce8
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-07-25 09:36:48 +0200

    adjust parameters to match "old" database

--- app/env.js
+++ app/env.js
@@ -5,9 +5,9 @@
   // Setting this to false will disable console output
   window.__env.enableDebug = true;
 
-  // property names of feature id and name (relevant for all spatial features) - KomMonitor specific 
-  window.__env.FEATURE_ID_PROPERTY_NAME = "ID";
-  window.__env.FEATURE_NAME_PROPERTY_NAME = "NAME";
+  // property names of feature id and name (relevant for all spatial features) - KomMonitor specific
+  window.__env.FEATURE_ID_PROPERTY_NAME = "spatialUnitFeatureId";
+  window.__env.FEATURE_NAME_PROPERTY_NAME = "spatialUnitFeatureName";
 
   // API url
   window.__env.apiUrl = 'http://kommonitor.fbg-hsbo.de/management';
