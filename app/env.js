91781eaa7c993f648e116aeb27d0c5211ceda2d4
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-08-26 15:15:48 +0200

    fix spelling mistake

--- app/env.js
+++ app/env.js
@@ -170,7 +170,7 @@
   window.__env.wfsDatasets = [
     {
       title: "Bodennutzung - Bebauungsplanumringe",
-      description: "Umringe der Bebauungspl&auml;ne gem&auml;&szlig; geodaten.metropoleruhr.de. <b>WFS-Dienst unterst&uuml;tzt keine r&auml;lichen Filter. Daher m&uuml;ssen zwingend alle Features abgerufen werden</b>.",
+      description: "Umringe der Bebauungspl&auml;ne gem&auml;&szlig; geodaten.metropoleruhr.de. <b>WFS-Dienst unterst&uuml;tzt keine r&auml;umllichen Filter. Daher m&uuml;ssen zwingend alle Features abgerufen werden</b>.",
       url: "https://geodaten.metropoleruhr.de/inspire/bodennutzung/metropoleruhr?",
       featureTypeNamespace: "ms",
       featureTypeName: "bplan_stand",
