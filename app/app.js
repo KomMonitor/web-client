81b07dc7da66da859f66a0f9ff4481f37292df99
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-09-02 07:35:07 +0200

    add initial alert for IE and Edge browsers

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
+if (/MSIE 9/i.test(navigator.userAgent) || /MSIE 10/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+    // This is internet explorer 9, 10 or 11
+    window.alert('Internet Explorer erkannt. Für eine optimale Nutzung von KomMonitor nutzen Sie nach Möglichkeit die Browser Firefox oder Chrome.');
+}
+
+
+if (/Edge\/\d./i.test(navigator.userAgent)){
+   // This is Microsoft Edge
+
+   window.alert('Microsoft Edge erkannt. Für eine optimale Nutzung von KomMonitor nutzen Sie nach Möglichkeit die Browser Firefox oder Chrome.');
+}
 
 var env = {};
 
