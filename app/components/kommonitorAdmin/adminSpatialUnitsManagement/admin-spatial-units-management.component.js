080f42a0e893913b71ca7455eb35432488f0c38a
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-09-09 22:21:38 +0200

    first draft for spatial unit admin data tables

--- app/components/kommonitorAdmin/adminSpatialUnitsManagement/admin-spatial-units-management.component.js
+++ app/components/kommonitorAdmin/adminSpatialUnitsManagement/admin-spatial-units-management.component.js
@@ -6,6 +6,47 @@ angular.module('adminSpatialUnitsManagement').component('adminSpatialUnitsManage
 		// initialize any adminLTE box widgets
 	  $('.box').boxWidget();
 
+		$scope.loadingData = true;
+
+		$scope.availableSpatialUnitDatasets;
+		$scope.selectAllEntriesInput = false;
+
+		$scope.$on("initialMetadataLoadingCompleted", function (event) {
+
+			$scope.availableSpatialUnitDatasets = JSON.parse(JSON.stringify(kommonitorDataExchangeService.availableSpatialUnits));
+
+			// initialize properties
+			$scope.availableSpatialUnitDatasets.forEach(function(dataset){
+				dataset.isSelected = false;
+			});
+
+			$scope.loadingData = false;
+
+			// must use timeout as table content is just built up by angular
+			setTimeout(function(){
+				// initialize table as DataTable
+				$('#spatialUnitOverviewTable').dataTable( {
+							"language": {
+									"url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/German.json"
+							}
+					} );
+			}, 500);
+
+		});
+
+		$scope.onChangeSelectAllEntries = function(){
+			if ($scope.selectAllEntriesInput){
+				$scope.availableSpatialUnitDatasets.forEach(function(dataset){
+					dataset.isSelected = true;
+				});
+			}
+			else{
+				$scope.availableSpatialUnitDatasets.forEach(function(dataset){
+					dataset.isSelected = false;
+				});
+			}
+		};
+
 
 	}
 ]});
