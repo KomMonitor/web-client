f99ebd05216987108f66da6e967e3b92745cb357
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-09-11 15:35:08 +0200

    bla

--- app/env.js
+++ app/env.js
@@ -92,7 +92,7 @@
 
   // array of indicator name substring that shal be used to filter out / hide certain indicators by their name
   // e.g. set ["entwicklung"] to hide all indicators whose name contains the substring "entwicklung"
-  window.__env.arrayOfNameSubstringsForHidingIndicators = [];
+  window.__env.arrayOfNameSubstringsForHidingIndicators = ["Prozentuale", "Standardabweichung", "Stadtteil", "ausschließlich", "doppelt", "Versorgungsquote"];
 
   // e-mail recipient for feedback mail
   window.__env.feedbackMailRecipient = "thomas.blasche@amt62.essen.de";
