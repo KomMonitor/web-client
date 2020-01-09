113ac3a84aead12ecec2d25d1bf651e0a0a60f7e
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-01-09 21:42:13 +0100

    add library ui.bootstrap in order to enable bootstrap support for angular specific data structures such as ng-repeated collapsible items

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,8 @@ module.exports = {
           { from: nodeModulePathConstant + 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css', to: 'bootstrap-datepicker/css' },
           { from: nodeModulePathConstant + 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js', to: 'bootstrap-datepicker/js' },
           { from: nodeModulePathConstant + 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.de.min.js', to: 'bootstrap-datepicker/js' },
-          { from: nodeModulePathConstant + 'angular-datatables/dist', to: 'angular-datatables/' }
+          { from: nodeModulePathConstant + 'angular-datatables/dist', to: 'angular-datatables/' },
+          { from: nodeModulePathConstant + 'angular-ui-bootstrap/dist', to: 'angular-ui-bootstrap/' }
 
       ])
     ]
