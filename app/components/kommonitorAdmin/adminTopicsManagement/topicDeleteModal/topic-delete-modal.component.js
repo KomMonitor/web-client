1d7de6ebf12086c6ec0e03f3bb6ef1c32019c79f
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-01-13 13:11:58 +0100

    refactor method for prettyPrinting JSON

--- app/components/kommonitorAdmin/adminTopicsManagement/topicDeleteModal/topic-delete-modal.component.js
+++ app/components/kommonitorAdmin/adminTopicsManagement/topicDeleteModal/topic-delete-modal.component.js
@@ -16,7 +16,7 @@ angular.module('topicDeleteModal').component('topicDeleteModal', {
 			$scope.topicToDelete = topic;
 
 			// $scope.topicToDelete_prettyPrint = JSON.stringify(topic, null, 2); // spacing level = 2;
-			$scope.topicToDelete_prettyPrint = $scope.syntaxHighlight(topic);
+			$scope.topicToDelete_prettyPrint = kommonitorDataExchangeService.syntaxHighlightJSON(topic);
 
 			document.getElementById("deleteTopicPre").innerHTML = $scope.topicToDelete_prettyPrint;
 
@@ -70,28 +70,6 @@ angular.module('topicDeleteModal').component('topicDeleteModal', {
 			});
 
 		};
-
-		$scope.syntaxHighlight = function(json) {
-		    if (typeof json != 'string') {
-		         json = JSON.stringify(json, undefined, 2);
-		    }
-		    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-		    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-		        var cls = 'number';
-		        if (/^"/.test(match)) {
-		            if (/:$/.test(match)) {
-		                cls = 'key';
-		            } else {
-		                cls = 'string';
-		            }
-		        } else if (/true|false/.test(match)) {
-		            cls = 'boolean';
-		        } else if (/null/.test(match)) {
-		            cls = 'null';
-		        }
-		        return '<span class="' + cls + '">' + match + '</span>';
-		    });
-		}
 
 			$scope.hideSuccessAlert = function(){
 				$("#topicDeleteSuccessAlert").hide();
