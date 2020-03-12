7df572d41dda131c197e6f195e54b152ddab2a13
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-03-12 05:19:43 +0100

    comment out a line that is not required and causes errors in production

--- app/components/kommonitorAdmin/adminGeoresourcesManagement/georesourceEditMetadataModal/georesource-edit-metadata-modal.component.js
+++ app/components/kommonitorAdmin/adminGeoresourcesManagement/georesourceEditMetadataModal/georesource-edit-metadata-modal.component.js
@@ -686,7 +686,7 @@ angular.module('georesourceEditMetadataModal').component('georesourceEditMetadat
 						//1. $scope.scale current_fs down to 80%
 						$scope.scale = 1 - (1 - now) * 0.2;
 						//2. bring $scope.next_fs from the right(50%)
-						left = (now * 50)+"%";
+						// left = (now * 50)+"%";
 						//3. increase $scope.opacity of $scope.next_fs to 1 as it moves in
 						$scope.opacity = 1 - now;
 						$scope.current_fs.css({
