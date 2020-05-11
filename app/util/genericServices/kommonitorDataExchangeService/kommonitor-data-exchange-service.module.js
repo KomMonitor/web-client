14edb9a9a926374c2e5442e03d6a66eedb893912
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-05-11 23:26:39 +0200

    display timestamps with full timestamp

--- app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
+++ app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
@@ -1574,13 +1574,13 @@ angular
             * TODO FIXME dateSLider formatter will return only year for now to prevent misleading month and day settings
             */
   
-            return date.getFullYear();
+            // return date.getFullYear();
   
-            // return date.toLocaleDateString("de-DE", {
-            // 		year: 'numeric',
-            // 		month: 'long',
-            // 		day: 'numeric'
-            // });
-          }
+            return date.toLocaleDateString("de-DE", {
+            		year: 'numeric',
+            		month: 'long',
+            		day: 'numeric'
+            });
+          };
 
 				}]);
