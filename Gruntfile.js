7ee7a41a95e7090c950dec2f8cb228ea9b102399
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-08-11 10:15:37 +0200

    add config file to gruntfile to include it in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,8 @@ module.exports = function (grunt) {
             'components/**/*.template.html',
             'keycloak.json',
             'Datatables.Language.German.json',
-            'env.js'
+            'env.js',
+            'controls-config.json'
         ],
 
         // babel: {
