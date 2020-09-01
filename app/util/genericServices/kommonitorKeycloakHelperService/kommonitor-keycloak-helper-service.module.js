187c1f363be066d0c95b4c33eee33950bbc3f280
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-09-01 10:48:45 +0200

    add skeleton for role management (for now without fetching of keycloak roles on startup)

--- app/util/genericServices/kommonitorKeycloakHelperService/kommonitor-keycloak-helper-service.module.js
+++ app/util/genericServices/kommonitorKeycloakHelperService/kommonitor-keycloak-helper-service.module.js
@@ -13,45 +13,53 @@ angular
   .module('kommonitorKeycloakHelper', [])
   .service(
     'kommonitorKeycloakHelperService', ['$rootScope', '$timeout', 'kommonitorDataExchangeService', '$http', '__env',
-    async function ($rootScope, $timeout,
+    function ($rootScope, $timeout,
       kommonitorDataExchangeService, $http, __env) {
 
-        this.keycloakRoles = [];
-        this.targetUrlToKeycloakInstance = "";
-        this.realm = "";
-        this.clientId = "";
+      var self = this;
+      this.keycloakRoles = [];
+      this.targetUrlToKeycloakInstance = "";
+      this.realm = "";
+      this.clientId = "";
 
-        this.init = async function(){
-          await $http.get('keycloak.json').then(function (response) {
-            var keycloakConfig = JSON.parse(response.data);
+      this.init = async function () {
+        try {
+          await $http.get('keycloak.json').then(async function (response) {
+            var keycloakConfig = response.data;
             self.targetUrlToKeycloakInstance = keycloakConfig['auth-server-url'];
             self.realm = keycloakConfig['realm'];
             self.clientId = keycloakConfig['resource'];
-          });
-        };
 
-      await this.init();
-      
-      this.fetchRoles = async function(){
+            // self.keycloakRoles = await self.fetchRoles();
+          });
+        } catch (error) {
+          console.error("Error while initializing kommonitorKeycloakHelperService. Error whil fetching and interpreting config file. Error is: " + error);
+        }
+      };
+
+      this.fetchRoles = async function () {
+
+        console.log = "Fetching roles from Keycloak instance at " + this.targetUrlToKeycloakInstance;
         return await $http({
           url: this.targetUrlToKeycloakInstance + this.realm + "/clients/" + this.clientId + "/roles",
           method: "GET"
         }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-  
-            return response.data;
-  
-          }, function errorCallback(error) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-            //$scope.error = response.statusText;
-            console.error("Error while fetching roles from keycloak.");
-            kommonitorDataExchangeService.displayMapApplicationError(error);
+          // this callback will be called asynchronously
+          // when the response is available
+
+          return response.data;
+
+        }, function errorCallback(error) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          //$scope.error = response.statusText;
+          console.error("Error while fetching roles from keycloak.");
 
         });
       };
 
-      this.keycloakRoles = await this.fetchRoles();
+      this.init();
+
+
 
     }]);
