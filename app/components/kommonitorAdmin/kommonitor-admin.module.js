f65c192f006ff29c82c53f634190529d0ac3b87a
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-09-03 10:12:06 +0200

    implement an edit indicator spatial unit role mapping modal

--- app/components/kommonitorAdmin/kommonitor-admin.module.js
+++ app/components/kommonitorAdmin/kommonitor-admin.module.js
@@ -1,8 +1,8 @@
 angular.module('kommonitorAdmin', ['kommonitorDataExchange', 'adminDashboardManagement', 'adminRoleManagement', 'adminTopicsManagement',
-'adminSpatialUnitsManagement','adminIndicatorsManagement', 'adminGeoresourcesManagement', 'adminScriptManagement',
-'spatialUnitAddModal','spatialUnitEditMetadataModal', 'spatialUnitEditFeaturesModal','spatialUnitDeleteModal',
-'georesourceDeleteModal','georesourceAddModal', 'georesourceEditFeaturesModal', 'georesourceEditMetadataModal',
-'indicatorDeleteModal','indicatorAddModal', 'indicatorEditFeaturesModal', 'indicatorEditMetadataModal',
-'topicEditModal', 'topicDeleteModal', 'roleAddModal', 'roleDeleteModal', 'roleEditMetadataModal'
-// ,'spatialUnitDeleteModal',,
+    'adminSpatialUnitsManagement', 'adminIndicatorsManagement', 'adminGeoresourcesManagement', 'adminScriptManagement',
+    'spatialUnitAddModal', 'spatialUnitEditMetadataModal', 'spatialUnitEditFeaturesModal', 'spatialUnitDeleteModal',
+    'georesourceDeleteModal', 'georesourceAddModal', 'georesourceEditFeaturesModal', 'georesourceEditMetadataModal',
+    'indicatorDeleteModal', 'indicatorAddModal', 'indicatorEditFeaturesModal', 'indicatorEditMetadataModal', 'indicatorEditIndicatorSpatialUnitRolesModal',
+    'topicEditModal', 'topicDeleteModal', 'roleAddModal', 'roleDeleteModal', 'roleEditMetadataModal'
+    // ,'spatialUnitDeleteModal',,
 ]);
