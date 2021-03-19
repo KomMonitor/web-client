99684fd0d4d4786bf5917674b18fcd3be9186e9d
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2021-03-19 23:23:51 +0100

    add CSV export

--- app/components/kommonitorUserInterface/kommonitorControls/kommonitorLegend/kommonitor-legend.component.js
+++ app/components/kommonitorUserInterface/kommonitorControls/kommonitorLegend/kommonitor-legend.component.js
@@ -217,6 +217,120 @@ angular
 				  
 							shpwrite.download(geoJSON, options);
 						  };
+
+						  $scope.downloadIndicatorAsCSV = function () {
+				  
+							var fileName = kommonitorDataExchangeService.selectedIndicator.indicatorName + "_" + kommonitorDataExchangeService.selectedSpatialUnit.spatialUnitLevel;
+				  
+							var geoJSON;
+				  
+							if(kommonitorDataExchangeService.isBalanceChecked){
+							  geoJSON = jQuery.extend(true, {}, kommonitorDataExchangeService.indicatorAndMetadataAsBalance.geoJSON);
+							  fileName += "_Bilanz_" + $scope.currentIndicatorMetadataAndGeoJSON['fromDate'] + " - " + $scope.currentIndicatorMetadataAndGeoJSON['toDate'];
+							}
+							else{
+							  geoJSON = jQuery.extend(true, {}, kommonitorDataExchangeService.selectedIndicator.geoJSON);
+							  fileName += "_" + kommonitorDataExchangeService.selectedDate;
+							}
+
+							var items = [];
+				  
+							for (var feature of geoJSON.features) {
+							  var properties = feature.properties;
+				  
+							  // rename all properties due to char limit in shaoefiles
+							  var keys = Object.keys(properties);
+				  
+							  for (var key of keys) {
+								var newKey = undefined;
+								if (key.toLowerCase().includes("featureid")) {
+								  newKey = "ID";
+								}
+								else if (key.toLowerCase().includes("featurename")) {
+								  newKey = "NAME";
+								}
+								else if (key.toLowerCase().includes("date_")) {
+								  // from DATE_2018-01-01
+								  // to 2018-01-01
+								  newKey = key.split("_")[1];
+								}
+								else if (key.toLowerCase().includes("startdate")) {
+								  newKey = "validFrom";
+								}
+								else if (key.toLowerCase().includes("enddate")) {
+								  newKey = "validTo";
+								}
+				  
+								if (newKey) {
+								  properties[newKey] = properties[key];
+								  delete properties[key];
+								}
+							  }
+				  
+							  // replace properties with the one with new keys
+							  feature.properties = properties;
+
+							  items.push(properties);
+							}
+				  
+							var headers = {};
+
+							for (const key in items[0]) {
+								if (Object.hasOwnProperty.call(items[0], key)) {
+									headers[key] = key;									
+								}
+							}
+
+							exportCSVFile(headers, items, fileName);
+						  };
+
+						  function convertToCSV(objArray) {
+							var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+							var str = '';
+						
+							for (var i = 0; i < array.length; i++) {
+								var line = '';
+								for (var index in array[i]) {
+									if (line != '') line += ','
+						
+									line += array[i][index];
+								}
+						
+								str += line + '\r\n';
+							}
+						
+							return str;
+						}
+						
+						function exportCSVFile(headers, items, fileTitle) {
+							if (headers) {
+								items.unshift(headers);
+							}
+						
+							// Convert Object to JSON
+							var jsonObject = JSON.stringify(items);
+						
+							var csv = convertToCSV(jsonObject);
+						
+							var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+						
+							var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+							if (navigator.msSaveBlob) { // IE 10+
+								navigator.msSaveBlob(blob, exportedFilenmae);
+							} else {
+								var link = document.createElement("a");
+								if (link.download !== undefined) { // feature detection
+									// Browsers that support HTML5 download attribute
+									var url = URL.createObjectURL(blob);
+									link.setAttribute("href", url);
+									link.setAttribute("download", exportedFilenmae);
+									link.style.visibility = 'hidden';
+									document.body.appendChild(link);
+									link.click();
+									document.body.removeChild(link);
+								}
+							}
+						}
 				  
 
 					}]
