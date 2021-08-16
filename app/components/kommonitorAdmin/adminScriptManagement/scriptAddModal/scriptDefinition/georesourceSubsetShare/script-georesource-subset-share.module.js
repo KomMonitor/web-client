60aa08999c324bf9caecdc28f54091973cbea611
mschonlau <marcel.schonlau@hs-bochum.de>
2021-08-16 12:34:29 +0200

    add angularjs-dropdown-multiselect to module

--- app/components/kommonitorAdmin/adminScriptManagement/scriptAddModal/scriptDefinition/georesourceSubsetShare/script-georesource-subset-share.module.js
+++ app/components/kommonitorAdmin/adminScriptManagement/scriptAddModal/scriptDefinition/georesourceSubsetShare/script-georesource-subset-share.module.js
@@ -1,1 +1,1 @@
-angular.module('scriptGeoresourceSubsetShare', ['kommonitorDataExchange', 'kommonitorScriptHelper']);
+angular.module('scriptGeoresourceSubsetShare', ['kommonitorDataExchange', 'kommonitorScriptHelper', 'angularjs-dropdown-multiselect']);
