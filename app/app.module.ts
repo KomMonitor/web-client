17501170dbc951f03271ce12fa9fe7228680619d
ashwanth511 <ashwanthstark1963@gmail.com>
2023-08-03 18:04:43 +0530

    updated bootstrapping code

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,28 @@
-import { DoBootstrap, NgModule } from '@angular/core';
+import { DoBootstrap, NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UpgradeModule } from '@angular/upgrade/static';
+import { UpgradeModule, setAngularJSGlobal } from '@angular/upgrade/static';
 import { InfoModalModule } from 'components/kommonitorUserInterface/kommonitorControls/infoModal/info-modal.module';
 import { VersionInfoModule } from 'components/kommonitorUserInterface/kommonitorControls/versionInfo/version-info.module';
+
+declare var angular: any; // Declare the AngularJS global variable
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule,
+    InfoModalModule,
+    VersionInfoModule
   ],
- 
+  // ...
 })
 export class AppModule implements DoBootstrap {
-    constructor(private upgrade: UpgradeModule) { }
-    ngDoBootstrap() {
-        this.upgrade.bootstrap(document.documentElement, ['kommonitorClient']);
-      }
+  constructor(private upgrade: UpgradeModule, private injector: Injector) {
+    setAngularJSGlobal(angular); // Set the AngularJS global object for @angular/upgrade/static
+  }
+
+  ngDoBootstrap() {
+    // Bootstrap the AngularJS application
+    const angularJSModule = angular.module('kommonitorClient', []);
+    this.upgrade.bootstrap(document.documentElement, [angularJSModule.name], { strictDi: false });
+ }
 }
