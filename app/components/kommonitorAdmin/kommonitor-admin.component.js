667661fe16dc81e751e2cda86c9696ae0cd499f2
cWagner52N <c.wagner@52north.org>
2024-08-07 08:41:12 +0200

    admin page, user info view groups and roles redesigned

--- app/components/kommonitorAdmin/kommonitor-admin.component.js
+++ app/components/kommonitorAdmin/kommonitor-admin.component.js
@@ -13,6 +13,9 @@ angular
 
 								$scope.activeItemBackupId = "adminDashboardNavItem";
 
+                $scope.userRoleInformation = {};
+                $scope.userGroupInformation = [];
+
 								// initialize any adminLTE box widgets
 							  $('.box').boxWidget();
 
@@ -30,7 +33,42 @@ angular
 									// 	  this.checkAuthorizationOnStartup_withoutKeycloak();
 									// }
 									kommonitorDataExchangeService.fetchAllMetadata();
+
+                  setTimeout(function(){
+										$scope.prepUserInformation();
+									}, 1000);
 								};
+
+                $scope.prepUserInformation = function() {
+
+                  if(kommonitorDataExchangeService.currentKomMonitorLoginRoleNames.length>0) {
+                    kommonitorDataExchangeService.currentKomMonitorLoginRoleNames.forEach(roles => {
+                      
+                      let key = roles.split('.')[0];
+                      let role = roles.split('.')[1];
+
+                      if(!$scope.userRoleInformation.hasOwnProperty(key)) {
+                        $scope.userRoleInformation[key] = [];
+                      }
+                      
+                      $scope.userRoleInformation[key].push(role);
+
+                    });
+                  }
+
+                  if(kommonitorDataExchangeService.currentKeycloakLoginGroups.length>0) {
+                    kommonitorDataExchangeService.currentKeycloakLoginGroups.forEach((group, index) => {
+
+                      let parts = group.split('/');
+                      $scope.userGroupInformation[index] = [];
+
+                      parts.forEach(part => {
+                        if(part.length>0)
+                          $scope.userGroupInformation[index].push(part);
+                      });
+                    });
+                  }
+                }
 
 								this.init();
 
