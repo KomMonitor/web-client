35def6cc27bb8eed828b253f08ac2d1c8b6b4090
cDanowski <christian.danowski-buhren@hs-bochum.de>
2024-11-11 21:54:22 +0100

    adjust widget control menu due to v4 mandant data model changes. roles must be replaced by group names

--- app/components/kommonitorAdmin/adminConfig/adminControlsConfig/admin-controls-config.component.js
+++ app/components/kommonitorAdmin/adminConfig/adminControlsConfig/admin-controls-config.component.js
@@ -14,7 +14,7 @@ angular.module('adminControlsConfig').component('adminControlsConfig', {
 		$scope.missingRequiredParameters = [];
 		$scope.missingRequiredParameters_string = "";
 
-		$scope.keywordsInConfig = ["id", "roles", "indicatorConfig", "poi", "dataImport", "filter", 
+		$scope.keywordsInConfig = ["id", "groups", "indicatorConfig", "poi", "dataImport", "filter", 
 		"measureOfValueClassification", "balance", "diagrams", "radarDiagram", "regressionDiagram", 
 		"reachability", "processing", "indicatorLegendExportButtons", "reportingButton", "diagramExportButtons",
 		"georesourceExportButtons"];
