ecf790e65110370559a4c30df7ef381d2aafed5b
cDanowski <christian.danowski-buhren@hs-bochum.de>
2025-01-10 09:36:29 +0100

    finally fix sum and share scripts regarding NULL value handling

--- app/kommonitor-script-resources/km_indicator_percentage.js
+++ app/kommonitor-script-resources/km_indicator_percentage.js
@@ -114,6 +114,8 @@ async function computeIndicator(targetDate, targetSpatialUnit_geoJSON, baseIndic
 
       KmHelper.log("Process computation indicator");
 
+      let containsNullValues = false;
+
       /**
       * iterate over each feature of the baseIndicator and use its indicator value to modify map object
       * NOTE use spatialUnitFeatureId as key to be able to identify entries by their unique feature id!
@@ -121,6 +123,9 @@ async function computeIndicator(targetDate, targetSpatialUnit_geoJSON, baseIndic
 	  computationGeoJSON_array.forEach(function(geoJSON) {
 		
 		  geoJSON.features.forEach(function(feature) {
+
+        containsNullValues = false;
+
 			  // get the unique featureID of the spatial unit feature as String
 			  var featureId = KmHelper.getSpatialUnitFeatureIdValue(feature);
 			  // get the time series value of the base indicator feature for the requested target date (with its required prefix!)
@@ -129,6 +134,7 @@ async function computeIndicator(targetDate, targetSpatialUnit_geoJSON, baseIndic
 				  KmHelper.log("WARNING: the feature with featureID '" + featureId + "' does not contain a time series value for targetDate '" + targetDate + "'");
 				  KmHelper.log("WARNING: the feature value will thus be set to 'NULL' and computation will abort");
 				  partValue = null;
+          containsNullValues = true;
 				  }
 				  // modify map object (i.e. set value initially, or perform calculations and store modified value)
           // key should be unique featureId of the spatial unit feature
@@ -139,20 +145,29 @@ async function computeIndicator(targetDate, targetSpatialUnit_geoJSON, baseIndic
                 featureId: featureId,
                 indicatorValue: undefined,
                 refValue: null,
-				        intermediateValue: 0
+				        intermediateValue: 0,
+                containsNullValues: containsNullValues
                 };
  
             map.set(featureId, mapObject);    
         }  
 
-				  var mapEntry = map.get(featureId);  
-				  if(partValue == null || mapEntry.intermediateValue == null){
-            mapEntry.intermediateValue = null;
+				  var mapEntry = map.get(featureId);
+          if(containsNullValues){
+            mapEntry.containsNullValues = true;
+          }
+  
+          // only compute if there are values != null
+          if(!mapEntry.containsNullValues){
+            mapEntry.intermediateValue = mapEntry.intermediateValue + partValue;
+            map.set(featureId, mapEntry);
           }
           else{
-            mapEntry.intermediateValue = mapEntry.intermediateValue + partValue;
+            mapEntry.intermediateValue = null;
+            mapEntry.partValue = null;
+            mapEntry.indicatorValue = null;
+            map.set(featureId, mapEntry);          
           }
-				  map.set(featureId, mapEntry);
 				  });				 
 				  });
       	
