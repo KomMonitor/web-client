167210acc7a3c93d528ae9d08dc2a4c60a80f21b
Sebastian Drost <s.drost@52north.org>
2025-03-18 11:36:27 +0100

    Fix undefined check

--- app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
+++ app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
@@ -651,7 +651,7 @@ angular
           this.modifyIndicators = function(indicators) {
 
             var decimalDefault = 2;
-            if(__env.numberOfDecimals)
+            if(__env.numberOfDecimals !== undefined)
               decimalDefault = __env.numberOfDecimals;
 
             indicators.forEach(elem => {
