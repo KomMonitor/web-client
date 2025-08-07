dbed5cfe7cdbe1dd11623d87444980805b3eb00d
cWagner52N <c.wagner@52north.org>
2025-08-07 16:15:38 +0200

    kommonitorRadar styling fixes, completed

--- app/components/ngComponents/userInterface/sidebar/indicatorRadar/indicator-radar.component.ts
+++ app/components/ngComponents/userInterface/sidebar/indicatorRadar/indicator-radar.component.ts
@@ -14,6 +14,9 @@ export class IndicatorRadarComponent implements OnInit {
 
   
   activeTab = 0;
+
+  isRadarChartCollapsed = false;
+  isIndicatorSelectCollapsed = false;
   
   date;
   spatialUnitName;
