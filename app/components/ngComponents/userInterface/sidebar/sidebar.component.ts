e479aacfe085a681c7984add96aed0a2850d9cd2
cWagner52N <c.wagner@52north.org>
2025-11-04 08:44:33 +0100

    expert toolbar design streamlined (partially)

--- app/components/ngComponents/userInterface/sidebar/sidebar.component.ts
+++ app/components/ngComponents/userInterface/sidebar/sidebar.component.ts
@@ -22,7 +22,7 @@ export class SidebarComponent implements OnInit{
 
   ngOnInit(): void {
     // default open
-    this.element = 'sidebarDiagramsCollapse';
+    //this.element = 'sidebarDiagramsCollapse';
   }
 
   closeSidebar() {
