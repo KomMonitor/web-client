379a9ff99d413bd2b244a9ef5fe01a7d60eff55d
Judith Kirstein <judith.kirstein@hs-bochum.de>
2026-05-08 14:02:45 +0200

    radar diagram updates on unselectAllFeatures

--- app/components/kommonitorUserInterface/kommonitorMap/kommonitor-map.component.js
+++ app/components/kommonitorUserInterface/kommonitorMap/kommonitor-map.component.js
@@ -3254,6 +3254,7 @@ angular.module('kommonitorMap').component(
 
           kommonitorFilterHelperService.clearSelectedFeatures();
           $rootScope.$broadcast("restyleCurrentLayer", false);
+          $rootScope.$broadcast("updateRadarDiagram", $scope.currentIndicatorMetadataAndGeoJSON, kommonitorDataExchangeService.selectedSpatialUnit.spatialUnitLevel, kommonitorDataExchangeService.selectedSpatialUnit.spatialUnitId, $scope.date);
         });
 
         $scope.$on("removeAllDrawnPoints", function (event) {
