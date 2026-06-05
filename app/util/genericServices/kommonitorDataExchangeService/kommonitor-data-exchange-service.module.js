b0572d524c58ceba4072569a336012bb046b4526
Sebastian Drost <s.drost@52north.org>
2026-06-05 10:41:45 +0200

    Add configuration for making filter config visible for resource creators

--- app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
+++ app/util/genericServices/kommonitorDataExchangeService/kommonitor-data-exchange-service.module.js
@@ -3194,6 +3194,16 @@ angular
       return false;
     }
 
+    this.checkGlobalFilterEditPermission = function(){
+      if(__env.showFilterConfigForGeodataEditRoleNames) {
+        return this.checkResourcesEditPermission();
+      }
+      else {
+        return this.checkAdminPermission();
+      }
+    }
+
+
     this.getRoleTitles = function(){
 
       return this.currentKeycloakLoginRoles.map(role => role.split('.')[role.split('.').length-1]);
