f018921664318dee983b998f68d4199e1f2bef29
Judith Kirstein <judith.kirstein@hs-bochum.de>
2026-07-20 16:22:44 +0200

    show class label in tooltip

--- app/services/feature-popup-helper-service/feature-popup-helper.service.ts
+++ app/services/feature-popup-helper-service/feature-popup-helper.service.ts
@@ -32,7 +32,8 @@ export class FeaturePopupHelperService {
   }
 
   /** `<b>Name</b><br/>value [unit]` tooltip used for indicator features. */
-  buildIndicatorTooltip(featureName: any, indicatorValueText: any, unitText: any): string {
-    return '<b>' + featureName + '</b><br/>' + indicatorValueText + ' [' + unitText + ']';
+  buildIndicatorTooltip(featureName: any, indicatorValueText: any, unitText: any, label?: any): string {
+    let optionalLabel = label ? '<br/>' + label : '';
+    return '<b>' + featureName + '</b><br/>' + indicatorValueText + ' [' + unitText + ']' + optionalLabel;
   }
 }
